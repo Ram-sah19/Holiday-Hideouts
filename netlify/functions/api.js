205bf11578dfeaa1aff6eb84d0434c43e6f2d43c
Rambilas Sah <ram6070246@gmail.com>
2026-06-26 10:19:11 +0530

    bugs fixed

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -3,4 +3,30 @@ require("dotenv").config();
 const serverless = require("serverless-http");
 const app = require("../../app.js");
 
-module.exports.handler = serverless(app);
+const handler = serverless(app, {
+    binary: ["multipart/form-data", "image/*"]
+});
+
+module.exports.handler = async (event, context) => {
+    // Get the request Content-Type header
+    const contentType = (
+        event.headers["content-type"] ||
+        event.headers["Content-Type"] ||
+        ""
+    ).toLowerCase();
+
+    // Decode the base64 body if it is text/urlencoded/json
+    if (event.isBase64Encoded && event.body) {
+        const isTextLike = 
+            contentType.includes("application/x-www-form-urlencoded") ||
+            contentType.includes("application/json") ||
+            contentType.includes("text/");
+
+        if (isTextLike) {
+            event.body = Buffer.from(event.body, "base64").toString("utf8");
+            event.isBase64Encoded = false;
+        }
+    }
+
+    return handler(event, context);
+};
